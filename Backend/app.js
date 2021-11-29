e9525db97930461eb1a23c57ff1df39c6a75b374
Jessica PAQUET <jessica-paquet@live.fr>
2021-11-29 16:17:19 +0100

    npm

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const dotenv = require('dotenv').config()
+const database = require("./DATABASE_CONNECTION");
+const cors = require('cors');
+const helmet = require('helmet');
 
 
-
-
-
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -13,6 +15,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(helmet());
 app.use(bodyParser.json());
 
 
