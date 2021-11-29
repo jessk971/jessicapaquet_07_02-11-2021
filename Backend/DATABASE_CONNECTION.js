1391a94bc7fb56bd136d92515fc1b0feba5bade9
Jessica PAQUET <jessica-paquet@live.fr>
2021-11-29 16:00:18 +0100

    database

---
+++ Backend/DATABASE_CONNECTION.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+require('dotenv').config()
+
+const sequelize = new Sequelize('groupomania', process.env.DB_NAME, process.env.DB_MDP, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    operatorsAliases: false
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch((err) => console.log('Unable to connect to the database:', err));
+
+module.exports = sequelize;
