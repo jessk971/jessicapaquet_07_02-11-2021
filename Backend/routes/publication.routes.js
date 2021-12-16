c24b5c06765c7539199bd91381dc2e6e09cf7161
Jessica PAQUET <jessica-paquet@live.fr>
2021-12-17 00:24:49 +0100

    routes publication

--- Backend/routes/publication.routes.js
+++ Backend/routes/publication.routes.js
@@ -7,5 +7,7 @@ const multer = require('../middleware/multer-config');
 router.post('/', auth, multer, publication.createPublication);
 router.get('/', auth, publication.getAllPublications);
 router.get('/:id', auth, publication.getOnePublication);
+router.put('/:id', auth, multer, publication.modifyPublication);
+router.delete('/:id', auth, publication.deletePublication);
 
 module.exports = router;
